eac4395635c9cea2188bdf8b075009a310a775be
Keven Leone <keven.leone@liferay.com>
2022-11-17 10:27:16 -0300

    Use trustable api from SDK

--- apps/web/src/main.ts
+++ apps/web/src/main.ts
@@ -1,11 +1,20 @@
-import "./style.css";
-import IncognitoSDK from "@incognito/sdk";
+import './style.css';
+import IncognitoSDK from '@incognito/sdk';
 
 const incognito = new IncognitoSDK(
-  "incognito-id-fff",
-  "https://incognito-api.incognito-poc.workers.dev"
+  'incognito-id-fff',
+  'https://incognito-api.incognito-poc.workers.dev'
 );
 
 incognito.init();
 
-document.getElementById("device-id")!.innerText = incognito.getDeviceId();
+document.getElementById('device-id')!.innerText = incognito.getDeviceId();
+
+const button = document.querySelector('button');
+const isTrustableDeviceSpan = document.getElementById('is-trustable-device');
+
+button!.onclick = async () => {
+  const { trustableDevice } = await incognito.getDeviceIsTrustable();
+
+  isTrustableDeviceSpan!.textContent = trustableDevice ? 'yes' : 'no';
+};
